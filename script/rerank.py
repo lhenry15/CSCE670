9d52d844390d03d49719cacbc131416636b2dcf3
Henry <henry@main-10-231-170-174.tamulink.tamu.edu>
2019-05-02 14:36:22 -0500

    add scripts

--- script/rerank.py
+++ script/rerank.py
@@ -1,43 +1,101 @@
-import xgboost
+import xgboost as xgb
+import sys
+from xgboost import XGBRanker
+import pickle
+from collections import defaultdict
+import numpy as np 
 
-def rerank(user, emb, rec_list, ground_truth):
-    build_train(emb, user, rec_list, ground_truth)
-    
+#def rerank(user, emb, rec_list, ground_truth):
+#    build_train(emb, user, rec_list, ground_truth)
 
 def read_train_data():
-    train = defaultdict(lambda: defaultdict(list))
+    rank_dict = defaultdict(lambda: defaultdict(int))
+    train = defaultdict(list)
     ground_truth = defaultdict(lambda: defaultdict())
     count = 0
+    d_train = []
+    users = []
+    items = []
+    print("Load File")
     with open("../data/train.csv", "r") as f:
+        num_lines = sum(1 for line in f)
+        f.seek(0)
         next(f)
         for line in f:
+            count += 1
+            sys.stdout.write("\rProgress:"+str(round(float(count)/num_lines, 4))+"\r")
+            sys.stdout.flush()
             line = line.strip().split(",")
             user = line[0]
             session = line[1]
             timestamp = line[2]
             action = line[4]
+            item = line[5]
+            users.append(user)
+            items.append(line[5])
             if "interaction" in action:
-                item = line[5]
-                train[user][session].append(item)
-                train[user][session] = list(set(train[user][session]))
+                rank_dict[user][item] += 1
+            if "search" in action:
+                rank_dict[user][item] += 5
             if "clickout item" in action:
-                if user in train.keys() and session in train[user].keys():
-                    count += 1
-                    item = line[5]
-                    ground_truth[user][session] = (timestamp, item)
-    return train, ground_truth
+                rank_dict[user][item] += 10
 
+    max_score = max([rank_dict[user][item] for user in rank_dict.keys() for item in rank_dict[user]])
+    min_score = min([rank_dict[user][item] for user in rank_dict.keys() for item in rank_dict[user]])
+    level = (max_score - min_score) / 25.0
 
-def train_rerank():
-    pass
+    print("Build Train File")
+    cnt = 0.0
+    for user in rank_dict.keys():
+        cnt += 1
+        sys.stdout.write("\rProgress:"+str(round(float(cnt)/len(rank_dict.keys()), 10))+"\r")
+        sys.stdout.flush()
+        for item in rank_dict[user].keys():
+            rk = int(rank_dict[user][item] / level)
+            train[user].append((item, rk))
+    return train
 
 def read_features(f_path):
     feature = defaultdict(list)
+    count = 0
     with open(f_path, "r") as f:
         for line in f:
+            count += 1
             line = line.strip().split(" ")
             feature[line[0]] = np.array([float(each) for each in line[1:]])
     return feature 
+
+def XGBRank(Xtrain, Ytrain, Xtest, Ytest):
+	dtrain = xgb.DMatrix(Xtrain, label=Ytrain)
+	dtrain.set_group([len(Ytrain)])
+	dtest = xgb.DMatrix(Xtest, label=Ytest)
+	dtest.set_group([len(Ytest)])
+	param = {
+		'max_depth':50,
+		'eta':0.05,
+		'gamma':0.01,
+		'min_child_weight':0.01,
+		'subsample':0.7,
+		'colsample_bytree':0.5,
+		'silent':1,
+		#'objective':'binary:logistic',
+		#'eval_metric':'precision@10',
+		#'eval_metric':'auc',
+		'objective':'rank:pairwise',
+		'eval_metric':'ndcg@25',
+		'random_state':1337
+		}
+	num_round = 1000
+	watch_list = [(dtest, 'eval'), (dtrain, 'train')]
+	bst = xgb.train(param, dtrain, num_round, watch_list)
+	test_pred = bst.predict(dtest)
+	train_pred = bst.predict(dtrain)
+	###without watch list
+	#bst = xgb.train(param, dtrain, num_round)
+	###	
+	test_pred = bst.predict(dtest)
+	train_pred = bst.predict(dtrain)
+	return bst
 
 def avg_emb(items, feature):
     count = 0
@@ -49,12 +107,26 @@ def avg_emb(items, feature):
     vec = vec / count
     return vec
 
-def build_train(data, feature, ground_truth):
+def build_train(data, feature):
+    X = []
+    Y = []
     for user in data.keys():
-        for sess in data[user].keys()
+        for each in data[user]:
+            item = each[0]
+            rank = each[1]
+            if user in feature.keys() and item in feature.keys():
+                user_rep = feature[user]
+                item_rep = feature[item]
+                x = np.concatenate((user_rep, item_rep), axis=None)
+                y = rank
+                X.append(x)
+                Y.append(y)
 
+    train = int( len(X) * 0.8 )
+    return np.array(X[:train]), np.array(Y[:train]), np.array(X[train:]), np.array(Y[train:])
 
-if __name__ == "__main__":
-    train_data, ground_truth = read_train_data()
-    feature = read_features("../features/mixed_item.rep")
-    build_train(train_data, feature, ground_truth)
+#if __name__ == "__main__":
+#    train_data = read_train_data()
+#    feature = read_features("../features/mixed.rep")
+#    Xtrain, Ytrain, Xtest, Ytest = build_train(train_data, feature)
+#    BST = XGBRank(Xtrain, Ytrain, Xtest, Ytest )
--- script/validate.py
+++ script/validate.py
@@ -1,9 +1,20 @@
 from collections import defaultdict
+from sklearn import preprocessing
+from annoy import AnnoyIndex
 import numpy as np
-from scipy.spatial.distance import cosine
 from tqdm import tqdm
 import sys
-from rerank import rerank
+#from rerank import rerank
+def normalize(feature):
+    X = []
+    for key in feature.keys():
+        X.append(feature[key])
+    X = np.array(X)
+    X = preprocessing.normalize(X)
+    for i, x in enumerate(X): 
+        key = list(feature.keys())[i]
+        feature[key] = x
+    return feature
 
 def read_train_data():
     train = defaultdict(lambda: defaultdict(list))
@@ -27,21 +38,44 @@ def read_train_data():
                     item = line[5]
                     ground_truth[user][session] = (timestamp, item)
             if count > 500:
-            #if count > 5:
                 break
     return train, ground_truth
 
-def read_features(f_path):
-    feature = defaultdict(list)
-    with open(f_path, "r") as f:
+def read_features(f1_path, f2_path):
+    #feature = defaultdict(list)
+    feature = {}
+    with open(f1_path, "r") as f:
         for line in f:
             line = line.strip().split(" ")
-            feature[line[0]] = np.array([float(each) for each in line[1:]])
-    return feature 
+            node_id = int(line[0])
+            vec1 = np.array([float(each) for each in line[1:]])
+            feature[node_id] = vec1 
+    
+    with open(f2_path, "r") as f:
+        for line in f:
+            line = line.strip().split(" ")
+            node_id = int(line[0])
+            vec2 = np.array([float(each) for each in line[1:]])
+            if node_id in feature.keys():
+                vec2 = np.concatenate((feature[node_id], vec2))
+                feature[node_id] = vec2 
+    feature = normalize(feature)
+    t = AnnoyIndex(len(feature[list(feature.keys())[0]]), metric='euclidean')
+    with open("embedding.csv", "w") as fw:
+        for _id in feature.keys():
+            fw.write(str(_id))
+            for col in feature[_id]:
+                fw.write(","+str(col))
+            fw.write("\n")
+    for nid in feature.keys():
+        t.add_item(nid, feature[nid])
+    print("Building Indexes...")
+    t.build(10)
+    return feature, t 
 
 def avg_emb(items, feature):
     count = 0
-    vec = np.zeros(len(feature[feature.keys[0]]).shape)
+    vec = np.zeros(feature[list(feature.keys())[0]].shape)
     for item in items:
         if item in feature.keys():
             count += 1
@@ -50,7 +84,7 @@ def avg_emb(items, feature):
     return vec
 
 # We can try to recommend by other kind of form
-def make_similarity_recommendation(data, feature): 
+def make_similarity_recommendation(data, feature, t): 
     rec_dict = defaultdict(lambda: defaultdict(list))
     cnt = 0.0
     users = tqdm(list(data.keys()))
@@ -65,11 +99,12 @@ def make_similarity_recommendation(data, feature):
                 if item in feature.keys():
                     item_vec = feature[item]
                 else:
-                    item_vec = avg_emb(data[user][sess][timestamp], feature)
+                    item_vec = avg_emb(data[user][sess], feature)
                     feature[item] = item_vec
-                candidate = find_top_k_items(item_vec, feature, 10) 
+                candidate = find_top_k_items(t, 10) 
                 rec_list.extend(candidate)
-            rec_dict[user][sess] = rerank(user, feature, rec_list, ground_truth[user][session])
+            rec_dict[user][sess] = rec_list
+            #rec_dict[user][sess] = rerank(user, feature, rec_list, ground_truth[user][session])
     return rec_dict
 
 def ReciprocalRank(item, rec_list):
@@ -89,7 +124,8 @@ def validate(rec, ground_truth):
                     time = ground_truth[user][sess][0]
                     item = ground_truth[user][sess][1]
                     val += ReciprocalRank(item, rec[user][sess])
-    val = val/cnt
+    if val > 0:
+        val = val/cnt
     return val
 
 #def cosine(vec1, vec2):
@@ -100,15 +136,18 @@ def validate(rec, ground_truth):
 #        val += v1*v2
 #    return val/(l1*l2)
 
-def find_top_k_items(item_vec, feature, k):
-    pairs = []
-    for i in feature.keys(): 
-        pairs.append((i, cosine(item_vec, feature[i])))
-    return [each[0] for each in sorted(pairs, key=lambda x:x[1], reverse=True)[:k]]
+def find_top_k_items(t, k):
+    #pairs.append((i, cosine(item_vec, feature[i])))
+    items = t.get_nns_by_item(0, k)
+    return items
+    #return [each[0] for each in sorted(pairs, key=lambda x:x[1], reverse=True)[:k]]
 
 if __name__ == "__main__":
     train_data, ground_truth = read_train_data()
-    feature = read_features("../features/mixed_item.rep")
-    rec = make_similarity_recommendation(train_data, feature)
+    #feature, t = read_features("../features/mixed_item.rep", "../10d_item_feature.csv")
+    #feature, t = read_features("../features/hoprec_item.rep", "../10d_item_feature.csv")
+    #feature, t = read_features("../features/hoprec_item.rep", "../features/nerank_item.rep")
+    feature, t = read_features("../features/nerank_item.rep", "../10d_item_feature.csv")#
+    rec = make_similarity_recommendation(train_data, feature, t)
     score = validate(rec, ground_truth)
     print("MRR:", score)
